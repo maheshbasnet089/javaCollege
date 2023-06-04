b3f2e3608c09c82f3b1f9806a9ee001f657c4156
maheshbasnet089 <basnetmanish089@gmail.com>
2023-06-04 10:00:28 +0545

    learn local,instance

---
+++ constructorDay/Test.java
@@ -0,0 +1,14 @@
+public class Test{
+     int i = 10 ;
+    public void pupAge(Integer age){
+        
+     
+        System.out.println(age);
+    }
+    public static void main(String[] args){
+        Test newTest = new Test();
+        System.out.println("The i value \t" + newTest.i);
+        /*Test newTest = new Test();
+        newTest.pupAge(10);*/
+    }
+}
